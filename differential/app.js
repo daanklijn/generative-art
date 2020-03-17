41a53d22bdb73f2bb7f38c2fca4b98a8c760bf07
Daan Klijn <daanklijn0@gmail.com>
2020-03-17 09:21:16 +0100

    First version of differential direction fields

--- differential/app.js
+++ differential/app.js
@@ -1,13 +1,71 @@
 console.log("HI")
-function setup() {
-    createCanvas(640, 480);
+let cols = 30, rows = 30, height = 300, width = 300;
+let x_min = -5, x_max= 5, y_min= -5, y_max= 5;
+let pathLenght = 100;
+var paths = [];
+
+function xToInterval(x){
+    stepSize = width/(x_max - x_min);
+    return x_min + (x/stepSize);
 }
 
-function draw() {
-    if (mouseIsPressed) {
-        fill(0);
-    } else {
-        fill(255);
+function yToInterval(y){
+    stepSize = height/(y_max - y_min);
+    return y_min + (y/stepSize);
+}
+
+function setup() {
+    var points = []
+    for (let y = 0; y < rows; y++) {
+        for (let x = 0; x < cols; x++) {
+            points.push([x * height/rows, y * width/cols])
+        }
     }
-    ellipse(mouseX, mouseY, 80, 80);
+    for (p of points) {
+        path = {
+            'path': [p],
+            'offset': Math.floor(Math.random() * pathLenght)
+        };
+        x = p[0];
+        y = p[1];
+        for (i = 0; i < pathLenght; i++) {
+            d = diff(xToInterval(x), yToInterval(y));
+            angle = slopeToAngle(d);
+            v = p5.Vector.fromAngle(angle, 1);
+            x = x + v.x;
+            y = y + v.y;
+            path['path'].push([x, y])
+        }
+        paths.push(path)
+    }
+
+    createCanvas(height, width);
+}
+
+function diff(x, y) {
+    return x/Math.sin(y);
+}
+
+function slopeToAngle(slope) {
+    return atan(slope);
+}
+
+
+function draw() {
+    background(0);
+    for (path of paths) {
+        offset = path['offset'];
+        color = -100+offset*2;
+        for (i = offset; i < pathLenght && i < offset + 20; i++) {
+            stroke(color);
+            color += 10;
+            x = path['path'][i][0];
+            y = path['path'][i][1];
+            point(x, y);
+        }
+        path['offset']++;
+        if(path['offset']>=pathLenght){
+            path['offset']=0
+        }
+    }
 }
