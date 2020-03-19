1ebb93dd73d7f158135f871850aebfc6bca835d7
Daan Klijn <daanklijn0@gmail.com>
2020-03-19 10:39:44 +0100

    Improve direction graph

--- differential/app.js
+++ differential/app.js
@@ -1,26 +1,27 @@
 console.log("HI")
 let cols = 30, rows = 30, height = 300, width = 300;
-let x_min = -5, x_max= 5, y_min= -5, y_max= 5;
-let pathLenght = 100;
+let x_min = -10, x_max= 10, y_min= -10, y_max= 10;
+let x_width = x_max - y_min;
+let y_height = y_max - y_min;
+let pathLenght = 200;
 var paths = [];
 
 function xToInterval(x){
-    stepSize = width/(x_max - x_min);
-    return x_min + (x/stepSize);
+    return (x - x_min)*(width/x_width);
 }
 
 function yToInterval(y){
-    stepSize = height/(y_max - y_min);
-    return y_min + (y/stepSize);
+    return (y - y_min)*(height/y_height);
 }
 
 function setup() {
     var points = []
     for (let y = 0; y < rows; y++) {
         for (let x = 0; x < cols; x++) {
-            points.push([x * height/rows, y * width/cols])
+            points.push([x_min + x * x_width/cols, y_min + y * y_height/rows])
         }
     }
+    // points.push([1 * (y_height/rows, 2 * (x_width/cols))]);
     for (p of points) {
         path = {
             'path': [p],
@@ -29,12 +30,18 @@ function setup() {
         x = p[0];
         y = p[1];
         for (i = 0; i < pathLenght; i++) {
-            d = diff(xToInterval(x), yToInterval(y));
-            angle = slopeToAngle(d);
-            v = p5.Vector.fromAngle(angle, 1);
-            x = x + v.x;
-            y = y + v.y;
-            path['path'].push([x, y])
+            // d = diff(x, y);
+            // dy = d/Math.sqrt(1+d**2);
+            // dx = 1/Math.sqrt(1+d**2);
+            dy1 = dy(x,y);
+            dx1 = dx(x,y);
+
+            dx2=dx1/Math.sqrt(dy1**2+dx1**2)*0.04;
+            dy2=dy1/Math.sqrt(dy1**2+dx1**2)*0.04;
+
+            x = x + dx2;
+            y = y + dy2;
+            path['path'].push([xToInterval(x),yToInterval(y)])
         }
         paths.push(path)
     }
@@ -42,23 +49,24 @@ function setup() {
     createCanvas(height, width);
 }
 
-function diff(x, y) {
-    return x/Math.sin(y);
+function dy(x, y) {
+    // return x + y*(x^2 + y^2)*Math.sin(Math.PI/(x^2 + y^2)^0.5)
+    return x*y;
 }
 
-function slopeToAngle(slope) {
-    return atan(slope);
+function dx(x, y) {
+    // return -y + x*(x^2 + y^2)*Math.sin(Math.PI/(x^2 + y^2)^0.5)
+    return x/y;
 }
-
 
 function draw() {
     background(0);
     for (path of paths) {
         offset = path['offset'];
         color = -100+offset*2;
-        for (i = offset; i < pathLenght && i < offset + 20; i++) {
+        for (i = offset; i < pathLenght && i < offset + 40; i++) {
             stroke(color);
-            color += 10;
+            color += 5;
             x = path['path'][i][0];
             y = path['path'][i][1];
             point(x, y);
